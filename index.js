d9aaad091e301df7fbe3c49d564ebcf8d3964bbe
averageWeb3-Contributer <joshweb3.0@gmail.com>
2022-10-15 18:55:27 +0100

    for you kind sir

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const express = require('express');
-const JSONdb = require('simple-json-db');
+import express from "express";
+import JSONdb from "simple-json-db";
+import { IPFSClient } from "./ipfs";
+
 const app = express();
-const db = JSONdb('./database.json')
+const db = JSONdb("./database.json");
+const ipfsClient = new IPFSClient();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// db.get(key)
+
+// db.set(key, value)
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/ipfs", async (req, res) => {
+  const hashes = req.body.hashes; // Array of IPFS CID paths
+  let values = [];
+
+  for (let i = 0; i < hashes.length; i++) {
+    const value = db.get(hashes[i]);
+    if (!value) {
+      const fetchedIpfsFile = await ipfsClient.getFile(hashes[i]);
+      db.set(hashes[i], fetchedIpfsFile);
+      values[i] = fetchedIpfsFile;
+    } else {
+      values[i] = value;
+    }
+  }
+
+  res.send(values);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
---
+++ ipfs.js
@@ -0,0 +1,54 @@
+import { create } from "ipfs-http-client";
+
+const getIpfsAuthHeader = () => {
+  if (!process.env.VUE_APP_PROJECT_ID)
+    throw new Error("PROJECT_ID not set in environment variables");
+  if (!process.env.VUE_APP_PROJECT_SECRET)
+    throw new Error("PROJECT_SECRET not set in environment variables");
+  return (
+    "Basic " +
+    Buffer.from(
+      process.env.VUE_APP_PROJECT_ID + ":" + process.env.VUE_APP_PROJECT_SECRET
+    ).toString("base64")
+  );
+};
+
+const baseInfuraURL = "https://ipfs.infura.io:5001/api/v0";
+
+class IPFSClient {
+  constructor() {
+    this.ipfsAPIClient = create({
+      host: "ipfs.infura.io",
+      port: 5001,
+      protocol: "https",
+      headers: {
+        authorization: getIpfsAuthHeader(),
+      },
+    });
+  }
+
+  getFile = (name) =>
+    new Promise((resolve) => {
+      const url = `${baseInfuraURL}/cat`;
+
+      let params = {
+        arg: name,
+      };
+
+      let headers = {
+        Authorization: getIpfsAuthHeader(),
+      };
+
+      let data = {};
+      network
+        .postRequest(url, params, headers, data)
+        .then((res) => {
+          resolve(res.value || null);
+        })
+        .catch((err) => {
+          resolve(null);
+        });
+    });
+}
+
+export default IPFSClient;
